8b4a040787d34efabca4b1f02668a0da0717d245
LiTingYao <18734821685@sina.cn>
2019-01-13 19:13:05 +0800

    添加spring-boot配置文件,及banner.txt的使用

--- demo/src/main/java/com/example/demo/ServletInitializer.java
+++ demo/src/main/java/com/example/demo/ServletInitializer.java
@@ -5,6 +5,11 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
 public class ServletInitializer extends SpringBootServletInitializer {
 
+	/**
+	 * 如此配置打包后,打成war 可以用tomcat下使用
+	 * @param application
+	 * @return
+	 */
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(DemoApplication.class);
